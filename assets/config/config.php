3ac14fa9b1fb2a3c401e17200974a3c4b739a27a
Wahyu Rusdiansyah <wahyurusdiansyah70@gmail.com>
2019-04-07 21:36:16 +0800

    Update config.php

--- assets/config/config.php
+++ assets/config/config.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect("localhost","root","Miftahulhuda","school");
+$conn = mysqli_connect("localhost","root","root","school");
 	if(mysqli_connect_errno()){
 		echo "can't connect to database : ".mysqli_connect_error();
 	}
